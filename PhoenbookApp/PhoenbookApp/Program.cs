2dc02ea11ac4c720f1a7e9374934a3544803ed9d
FelixBerget <felixberget@gmail.com>
2025-12-12 12:40:26 +0100

    Update Program.cs

--- PhoenbookApp/PhoenbookApp/Program.cs
+++ PhoenbookApp/PhoenbookApp/Program.cs
@@ -6,7 +6,7 @@
         {
             PhoneBook p = new PhoneBook();
             p.AddContact(new Contact("Felix", "11111111"));
-            p.AddContact(new Contact("Madher", "22222222"));
+            p.AddContact(new Contact("Mahder", "22222222"));
             p.AddContact(new Contact("Jonas", "33333333"));
             p.AddContact(new Contact("Kasper", "44444444"));
             p.RemoveContact("Felix");
