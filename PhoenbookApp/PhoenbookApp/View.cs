2593a93899ca7e0459441dd61f18756c87c62480
FelixBerget <felixberget@gmail.com>
2025-12-12 14:33:33 +0100

    Update View.cs

--- PhoenbookApp/PhoenbookApp/View.cs
+++ PhoenbookApp/PhoenbookApp/View.cs
@@ -24,6 +24,7 @@ namespace PhoenbookApp
             {
                 Console.WriteLine("Dette er phone book appen, her kan du finne kontakter");
                 Console.WriteLine("Skriv (søk) for å søke, (list) for å få liste og (avslutt) for å stoppe appen");
+                Console.WriteLine("Du kan også skrive (legg til) for å legge til ny bruker, og (fjern) for å fjerne en bruker");
                 string response = Console.ReadLine();
                 Console.Clear();
                 running = _validator.Validate(response, _answerer, _phonebook,_modifier);
